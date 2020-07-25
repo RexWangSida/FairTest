ee3cd640b339e55ffc8064c875c865e6e9eb1978
liuw56 <liuw56@mcmaster.ca>
2020-07-25 18:29:50 -0400

    added *

--- FairTest/pages/views.py
+++ FairTest/pages/views.py
@@ -3,8 +3,11 @@ from tests.models import Test
 from users.models import User
 import json
 # Create your views here.
+
+
 def index(request):
     return render(request, 'index.html')
+
 
 def login(request):
     return render(request, 'login.html')
@@ -20,7 +23,8 @@ def account(request, name):
             names.append(str(Test.objects.get(tid=i).name).strip())
             dates.append(str(Test.objects.get(tid=i).date))
             durations.append(str(Test.objects.get(tid=i).duration))
-        return render(request,'account.html', {'name': name, 'names':names, 'dates':dates, 'durations':durations})
+        return render(request, 'account.html', {'name': name, 'names': names, 'dates': dates, 'durations': durations})
+
 
 def testroom(request, name):
     if User.objects.filter(firstName=name).exists():
@@ -29,13 +33,11 @@ def testroom(request, name):
         for i in testL:
             testInfo = {
                 'name': str(Test.objects.get(tid=i).name).strip(),
-                'tid':str(Test.objects.get(tid=i).tid).strip(),
-                'status':int(Test.objects.get(tid=i).status),
-                'duration':int(Test.objects.get(tid=i).duration),
+                'tid': str(Test.objects.get(tid=i).tid).strip(),
+                'status': int(Test.objects.get(tid=i).status),
+                'duration': int(Test.objects.get(tid=i).duration),
                 'testSet': json.dumps(Test.objects.get(tid=i).content),
             }
             testInfos.append(json.dumps(testInfo))
 
-
-
-        return render(request,'TestRoom.html', {'name': name, 'testInfos':json.dumps({k:v for k, v in enumerate(lst)})}})
+        return render(request, 'TestRoom.html', {'name': name, 'testInfos': json.dumps({k: v for k, v in enumerate(testInfos)})})
