81408feb1aac0954e247fac2ee5e0deba6c02275
SidaWang <wangs132@mcmaster.ca>
2020-07-25 18:10:00 -0400

    json output completed

--- FairTest/pages/views.py
+++ FairTest/pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from tests.models import Test
 from users.models import User
+import json
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -22,4 +23,19 @@ def account(request, name):
         return render(request,'account.html', {'name': name, 'names':names, 'dates':dates, 'durations':durations})
 
 def testroom(request, name):
-    return render(request, 'TestRoom.html', {'name':name})
+    if User.objects.filter(firstName=name).exists():
+        testL = User.objects.get(firstName=name).regTests
+        testInfos = []
+        for i in testL:
+            testInfo = {
+                'name': str(Test.objects.get(tid=i).name).strip(),
+                'tid':str(Test.objects.get(tid=i).tid).strip(),
+                'status':int(Test.objects.get(tid=i).status),
+                'duration':int(Test.objects.get(tid=i).duration),
+                'testSet': json.dumps(Test.objects.get(tid=i).content),
+            }
+            testInfos.append(json.dumps(testInfo))
+
+
+
+        return render(request,'TestRoom.html', {'name': name, 'testInfos':json.dumps({k:v for k, v in enumerate(lst)})}})
