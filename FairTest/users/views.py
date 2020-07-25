03ae3da4adff8cd5ef49016eb9dc3274f152c65c
charlesZ-byte <zsmlvx@gmail.com>
2020-07-24 21:46:44 -0700

    view.py update

--- FairTest/users/views.py
+++ FairTest/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 import json
 from .models import User
+import csv
 # Create your views here.
 def loginAuth(request):
     if request.method == 'POST':
@@ -9,6 +10,9 @@ def loginAuth(request):
         if User.objects.filter(email=email).exists():
             if User.objects.get(email = email).password == password:
                 print(User.objects.get(email = email).firstName)
+                with open('TEMP.csv','w+') as file:
+                    writer=csv.writer(file)
+                    file.write(f'{email}')
                 return HttpResponse(json.dumps({'name':User.objects.get(email = email).firstName}), content_type="application/json")
         return HttpResponse(json.dumps({'msg':'The email you entered is not registered with us!'}), content_type="application/json")
     return render(request, 'login.html')
